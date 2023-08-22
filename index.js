a9644f044593d98358d4362e678db3f1eb8c0bad
nabiursiddique <nabiursiddique01@gmail.com>
2023-08-22 19:27:24 +0600

    api- Created api for getting all the posts

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,34 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7t8vw3l.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const postCollection = client.db('social-media-app').collection('all-posts');
+
+        // Getting all the posts
+        app.get('/posts', async (req, res) => {
+            const posts = await postCollection.find({}).toArray();
+            res.send(posts);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 
 // For testing the server
